e9d7afbe0410942f40b91b8ba091a83091fe9edb
Stephen Ro <1514330+royourboat@users.noreply.github.com>
2023-02-26 20:18:03 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-curl https://www.fire.ca.gov/umbraco/Api/IncidentApi/GetIncidents | jq . > incidents.json
+curl https://www.fire.ca.gov/umbraco/Api/IncidentApi/GetIncidents | jq . > incidents_test.json
